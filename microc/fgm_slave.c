42a1d55ea8772527c9ae48b9360a7d4afb4db866
Martin Howse <m@1010.co.uk>
2011-11-10 16:23:26 +0000

    spi and fgm tests

--- microc/fgm_slave.c
+++ microc/fgm_slave.c
@@ -14,16 +14,22 @@
 unsigned int fc_dur;
 unsigned long Freq;
 volatile unsigned char low;
-volatile unsigned int FreqCount, count;
+volatile unsigned int FreqCount, count, secondcount, secondfreq;
 
 ISR(INT0_vect) {
   if ((PIND & (1<<2)) == 4) count++; //PD2 as INT0 PIN
 }
 
+ISR(INT1_vect) {
+  if ((PIND & (1<<3)) == 8) secondcount++; //PD3 as INT1 PIN
+}
+
+
 ISR(TIMER1_COMPA_vect)	
 {
   FreqCount = count;
-  count=0;
+  secondfreq=secondcount;
+  count=0; secondcount=0;
 }
 
 
@@ -35,7 +41,6 @@ void WriteByteSPI(unsigned char byte)
 
 void InitSPI(void)
 {
-  DDRD = 0x08;
   DDRB = (1<<PB4);	 // Set MISO as OUT
   SPCR = ( (1<<SPE)|(1<<SPR1) |(1<<SPR0)|(1<<CPHA));//|(1<<SPIE));	// Enable SPI, Slave, set clock rate fck/128  
 }
@@ -50,17 +55,16 @@ void delay(int ms){
 void main() {
   unsigned int count,y,z;
   unsigned char inbetween;
-  unsigned int data;
+  unsigned int d1,d2,diff;
   InitSPI();
 
   // set up timer
   fc_dur=8;
   OCR1A = 91; // let's say every second! now every 128th
 
-  // TODO: enable interrupt on INT0 (and later on INT1)
-  GICR |= _BV(INT0); 
-  MCUCR |= (1<<ISC01);
-  MCUCR |= (1<<ISC00); 
+  // TODO: enable interrupt on INT0 and INT1
+  GICR |= (1<<INT0) | (1<<INT1); 
+  MCUCR |= (1<<ISC01) | (1<<ISC00) | (1<<ISC11) | (1<<ISC10); 
 
   TCCR1B |= (1 << WGM12);    // Mode 4, CTC on OCR1A
   TIMSK |= (1 << OCIE1A);     //Set interrupt on compare match
@@ -68,10 +72,13 @@ void main() {
   sei();
 
   while(1) {
-    data=FreqCount;
-    inbetween=(unsigned char)(data&0xff);
+    d1=FreqCount; d2=secondfreq;
+    if (d1>d2) diff=d1-d2;
+    else diff=d2-d1;
+
+    inbetween=(unsigned char)(diff&0xff);
     WriteByteSPI(inbetween);
-    inbetween=((unsigned int)(data)>>8)&0xff;
+    inbetween=((unsigned int)(diff)>>8)&0xff;
     WriteByteSPI(inbetween);
   }
 }
